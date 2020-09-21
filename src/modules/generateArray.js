8f339a6278acaba2b3e92ba88f962f9b21bda4a0
James <ericksen.github@gmail.com>
2020-09-21 17:42:10 +0100

    Graph working for new random numbers.

--- src/modules/generateArray.js
+++ src/modules/generateArray.js
@@ -1,14 +1,58 @@
+import { barChart } from "./graph";
+
 let newArray;
 
 const generateArray = () => {
   const length = Math.floor(Math.random() * 40) + 100;
   const maxNum = Math.floor(Math.random() * length);
 
-  let newArray = Array.from({ length: length }, () =>
+  newArray = Array.from({ length: length }, () =>
     Math.floor(Math.random() * maxNum)
   );
+  pushOneData();
+};
 
-  console.log(newArray);
+const pushOneData = () => {
+  let dataOne = [];
+  let xLabels = [];
+
+  newArray.forEach((ele) => {
+    dataOne.push(ele);
+    xLabels.push(ele);
+  });
+
+  let labelOne = "Random Number Array";
+  let borderColorOne = "rgb(106, 212, 134)";
+
+  chartOneDataset(dataOne, xLabels, labelOne, borderColorOne);
+};
+
+const chartOneDataset = (dataOne, xLabels, labelOne, borderColorOne) => {
+  const oneDataSet = {
+    datasets: [
+      {
+        data: dataOne,
+        label: labelOne,
+        borderColor: borderColorOne,
+      },
+    ],
+    labels: xLabels,
+  };
+
+  const oneAxis = {
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            display: false,
+          },
+        },
+      ],
+    },
+  };
+
+  barChart.data = oneDataSet;
+  barChart.options = oneAxis;
 };
 
 export { generateArray, newArray };
