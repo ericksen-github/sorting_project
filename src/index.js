f7629b23219d210fc7379fcef38175ab74fb26e3
James <ericksen.github@gmail.com>
2020-09-21 15:28:51 +0100

    Generating array of random numbers for graph.

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { generateArray } from "./modules/generateArray";
+
+generateArray();
---
+++ src/modules/generateArray.js
@@ -0,0 +1,12 @@
+const generateArray = () => {
+  const length = Math.floor(Math.random() * 40) + 100;
+  const maxNum = Math.floor(Math.random() * length);
+
+  let newArray = Array.from({ length: length }, () =>
+    Math.floor(Math.random() * maxNum)
+  );
+
+  console.log(newArray);
+};
+
+export { generateArray };
