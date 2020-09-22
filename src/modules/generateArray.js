af36b923ab3481b9f15ff1beffb1b68728b91a9f
James <ericksen.github@gmail.com>
2020-09-22 07:42:04 +0100

    Added function to create bars and display them on page.

--- src/modules/generateArray.js
+++ src/modules/generateArray.js
@@ -4,12 +4,22 @@ let newArray;
 
 const generateArray = () => {
   const minNum = 5;
-  const maxNum = 1000;
+  const maxNum = 500;
 
   newArray = Array.from({ length: 100 }, () =>
     Math.floor(Math.random() * (maxNum - minNum + 1) + minNum)
   );
-  console.log(newArray);
+  createBars(newArray);
+};
+
+const createBars = (newArray) => {
+  const barWrapper = document.getElementById("barWrapper");
+  for (let i = 0; i < newArray.length; i++) {
+    const newBar = document.createElement("div");
+    newBar.classList.add("bar");
+    newBar.style.height = `${newArray[i]}px`;
+    barWrapper.appendChild(newBar);
+  }
 };
 
 // const pushOneData = () => {
