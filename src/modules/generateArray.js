96df4d11143c0884aca331a74fb6aa98c8a45bdb
James <ericksen.github@gmail.com>
2020-09-22 07:44:09 +0100

    Removed remaining Chart.js code.

--- src/modules/generateArray.js
+++ src/modules/generateArray.js
@@ -1,5 +1,3 @@
-// import { barChart } from "./graph";
-
 let newArray;
 
 const generateArray = () => {
@@ -21,48 +19,5 @@ const createBars = (newArray) => {
     barWrapper.appendChild(newBar);
   }
 };
-
-// const pushOneData = () => {
-//   let dataOne = [];
-//   let xLabels = [];
-
-//   newArray.forEach((ele) => {
-//     dataOne.push(ele);
-//     xLabels.push(ele);
-//   });
-
-//   let labelOne = "Random Number Array";
-//   let borderColorOne = "rgb(106, 212, 134)";
-
-//   chartOneDataset(dataOne, xLabels, labelOne, borderColorOne);
-// };
-
-// const chartOneDataset = (dataOne, xLabels, labelOne, borderColorOne) => {
-//   const oneDataSet = {
-//     datasets: [
-//       {
-//         data: dataOne,
-//         label: labelOne,
-//         borderColor: borderColorOne,
-//       },
-//     ],
-//     labels: xLabels,
-//   };
-
-//   const oneAxis = {
-//     scales: {
-//       xAxes: [
-//         {
-//           ticks: {
-//             display: false,
-//           },
-//         },
-//       ],
-//     },
-//   };
-
-//   barChart.data = oneDataSet;
-//   barChart.options = oneAxis;
-// };
 
 export { generateArray, newArray };
