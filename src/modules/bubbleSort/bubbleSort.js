0c5be8383cb83049e9b67b8cc18f4331244a6bf3
James <ericksen.github@gmail.com>
2020-09-30 14:39:01 +0100

    Bubblesort working visually on DOM.

--- src/modules/bubbleSort/bubbleSort.js
+++ src/modules/bubbleSort/bubbleSort.js
@@ -1,4 +1,4 @@
-const timeController = 40; // 40 recommended
+const timeController = 10; // 40 recommended
 let timerTracker = 1;
 const bubbleSort = (() => {
   const preBubble = (unsortedArray) => {
@@ -11,16 +11,43 @@ const bubbleSort = (() => {
     do {
       swapped = false;
       for (let i = 0; i < len; i++) {
-        if (unsortedArray[i] > unsortedArray[i + 1]) {
-          let tmp = unsortedArray[i];
-          unsortedArray[i] = unsortedArray[i + 1];
-          unsortedArray[i + 1] = tmp;
+        if (unsortedArray[i + 1]) {
+          handleVisuals("red", unsortedArray[i], unsortedArray[i + 1], i);
 
-          swapped = true;
+          if (unsortedArray[i] > unsortedArray[i + 1]) {
+            let tmp = unsortedArray[i];
+            unsortedArray[i] = unsortedArray[i + 1];
+            unsortedArray[i + 1] = tmp;
+
+            handleVisuals("swapDiv", unsortedArray[i], unsortedArray[i + 1], i);
+            swapped = true;
+          }
+          handleVisuals("blue", unsortedArray[i], unsortedArray[i + 1], i);
         }
       }
     } while (swapped);
     return unsortedArray;
+  };
+
+  const handleVisuals = (type, first, second, i) => {
+    const barWrapper = document.getElementById("barWrapper");
+
+    const oneBar = barWrapper.childNodes[i];
+    const twoBar = barWrapper.childNodes[i + 1];
+
+    setTimeout(() => {
+      if (type == "red") {
+        oneBar.style.backgroundColor = "red";
+        twoBar.style.backgroundColor = "red";
+      } else if (type == "blue") {
+        oneBar.style.backgroundColor = "rgb(68, 68, 206)";
+        twoBar.style.backgroundColor = "rgb(68, 68, 206)";
+      } else if ((type = "swapDiv")) {
+        oneBar.style.height = `${first}px`;
+        twoBar.style.height = `${second}px`;
+      }
+    }, timeController * timerTracker);
+    timerTracker++;
   };
 
   return {
